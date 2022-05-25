7293a0a50ce1c79a3df217e9376aa1cefb15e006
fcadet <florian.kd@gmail.com>
2022-05-25 09:45:08 +0100

    test again

--- app/server.R
+++ app/server.R
@@ -5,9 +5,9 @@ server <- function(input, output) {
   output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
     x    <- divide_by_two(faithful[, 2])
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    hist(x, col = 'darkgray', border = 'white')
   })
 }
