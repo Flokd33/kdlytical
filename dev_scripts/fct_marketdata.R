a6ad73fa3bb15f96d57f3c0516742cf87a9c9318
fcadet <florian.kd@gmail.com>
2022-06-01 15:31:53 +0100

    formating

--- dev_scripts/fct_marketdata.R
+++ dev_scripts/fct_marketdata.R
@@ -2,19 +2,15 @@ require(quantmod)
 
 fct_marketdata_positions_yahoo <- function(list_tickers) {
   getSymbols(list_tickers, from="1997-12-31", src='yahoo')
-  
   return(df)
 }
 
 fct_marketdata_price_history_yahoo <- function(list_tickers, start, end) {
-
   getSymbols(list_tickers, from = start, to = end, src='yahoo')
   return(df)
-  
 }
 
 fct_marketdata_dividend_history_yahoo <- function(list_tickers, start, end) {
-  
   getSymbols(list_tickers, from = start, to = end, src='yahoo')
   return(df)
   
